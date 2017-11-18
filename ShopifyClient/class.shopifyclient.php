41d17959a92a00398cc09807071847c82820fbc6
Dima-Migunov <dmitri.migunov@gmail.com>
2017-11-18 15:46:15 +1000

    Delete class.shopifyclient.php

--- ShopifyClient/class.shopifyclient.php
+++
@@ -1,8 +0,0 @@
-<?php
-require dirname( __FILE__ ) . '/class.shopifycore.php';
-
-class ShopifyClient extends ShopifyCore{
-  public function __construct( $shop_domain, $token, $api_key, $secret ) {
-    parent::__construct( $shop_domain, $token, $api_key, $secret );
-  }
-}
