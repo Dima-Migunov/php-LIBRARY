893ce4c8f69841b0844cb25aba8adaf873c3bc7a
Dmitri Migunov <dmitri.migunov@gmail.com>
2018-03-25 12:46:06 +1000

    Update 18.03.25 new function GUID()

--- class.funs.php
+++ class.funs.php
@@ -335,6 +335,23 @@ class Funs {
 		return md5( $secret );
 	}
   
+  public static function GUID(  ){
+    if ( function_exists( 'com_create_guid' ) ) {
+      return com_create_guid();
+    }
+
+    mt_srand( (double) microtime() * 10000 );
+    $charid = strtoupper( md5( uniqid( strval( mt_rand() ), TRUE ) ) );
+    $hyphen = chr( 45 ); // "-"
+    $uuid   = substr( $charid, 0, 8 ) . $hyphen
+            . substr( $charid, 8, 4 ) . $hyphen
+            . substr( $charid, 12, 4 ) . $hyphen
+            . substr( $charid, 16, 4 ) . $hyphen
+            . substr( $charid, 20, 12 );
+    
+    return $uuid;
+  }
+  
   public static function random_hash( int $length ){
     if( $length < 1 ){
       return NULL;
