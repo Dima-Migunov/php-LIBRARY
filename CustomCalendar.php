39e75a622b8db3e66f9fcc27a98f9786b1633d11
Dmitri Migunov <dmitri.migunov@gmail.com>
2019-03-21 19:11:52 +0800

    Test Task 19.03.22 CustomCalendar.php

---
+++ CustomCalendar.php
@@ -0,0 +1,52 @@
+<?php
+
+class CustomCalendar{
+  private static
+    $monthsInYear       = 13,
+    $daysOfFirstMonth   = 22,
+    $daysOfSecondMonth  = 21,
+    $periodOfLeapYear   = 5,
+    $week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    $pointYear          = 1990,
+    $firstDayOfPointYear  = 'Monday',
+    $deltaDaysOfLeapYears = -1; // in days
+
+  public static function getDay( string $date ){
+    list( $days, $months, $year ) = explode( '.', $date );
+    
+    $days         = intval( $days );
+    $deltaYears   = intval( $year ) - self::$pointYear;
+    $leapYears    = floor( abs( $deltaYears / self::$periodOfLeapYear ) );
+    $monthAmount  = $deltaYears * self::$monthsInYear + intval( $months ) - 1;
+    $fmonths      = floor( abs( $monthAmount ) / 2 );
+    $smonths      = 0;
+    
+    if( $fmonths > 0 ){
+      $smonths  = abs( $monthAmount ) - $fmonths;
+    }
+    
+    if( $monthAmount >= 0 ){
+      $days += $fmonths * self::$daysOfFirstMonth
+              + $smonths * self::$daysOfSecondMonth
+              + $leapYears * self::$deltaDaysOfLeapYears;
+    }
+    else{
+      return 'Date in the Past ;)';
+    }
+
+    $weekday  = $days % count( self::$week ) - 1
+              + array_search( self::$firstDayOfPointYear, self::$week);
+    
+    return self::$week[ $weekday ];
+  }
+  
+  public static function daysInYear(){
+    $fmonths  = ceil( self::$monthsInYear / 2 );
+    $smonths  = self::$monthsInYear - $fmonths;
+    
+    return $fmonths * self::$daysOfFirstMonth + $smonths * self::$daysOfSecondMonth;
+  }
+}
+
+print_r( CustomCalendar::getDay( '17.11.2013' ) );
+
